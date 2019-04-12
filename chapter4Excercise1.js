08bbbbc1b9b8c0ff96d3921fb9c1e16568ae32bd
Muddy Donut <julian1559@gmail.com>
2019-04-11 20:04:01 -0400

    commit

--- chapter4Excercise1.js
+++ chapter4Excercise1.js
@@ -1,9 +1,18 @@
 
-function addEntry(x, y) {
+function range(x, y) {
     let list = [];
     for (let i = x; i<=y; i++) {
         list.push(i)
     }
     return list;
 }
-console.log(addEntry(1,10));
+
+function sum(...numbers) {
+    let result = numbers[i];
+    for (let i of numbers) {
+        if (numbers > result) result = number;
+    }
+    return result;
+}
+
+console.log(sum(range(1,10)));
