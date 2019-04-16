9b65f8255a9c24c8c4d462280db48fcf36363bd1
terskine <tinesha.erskine@gmail.com>
2019-04-15 21:48:18 -0400

    solved

--- chapter4Excercise1.js
+++ chapter4Excercise1.js
@@ -1,18 +1,17 @@
-
 function range(x, y) {
     let list = [];
     for (let i = x; i<=y; i++) {
-        list.push(i)
+        list.push(i);
     }
     return list;
 }
 
-function sum(...numbers) {
-    let result = numbers[i];
-    for (let i of numbers) {
-        if (numbers > result) result = number;
+function sum(list) {
+    let sum = 0;
+    for (let i = 0; i < list.length; i++) {
+        sum += list[i];
     }
-    return result;
+    return sum
 }
 
 console.log(sum(range(1,10)));
